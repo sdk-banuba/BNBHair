6e4f848422fbbc9b5b32ec5492b045022ce15e78
ci <ci@banuba.com>
2024-05-30 16:55:59 +0300

    1.13.2

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.13.0-1-g971efc62f"
+let bnbPackageVersion: Version = "1.13.2"
 
 let package = Package(
     name: "BNBHair",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBHair",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.13.0-1-g971efc62f/BNBHair.zip",
-            checksum: "f89874b2850c55f5f162b3c1f2f7ae6be9bd074069ee15966d9b6936d1b3dad5"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.13.2/BNBHair.zip",
+            checksum: "ab62e17f43446743149a06633abd6741fb49718f1c45e95b7fbe1e070c678940"
         ),
         .target(
             name: "BNBHair_BNBSdkCore",
